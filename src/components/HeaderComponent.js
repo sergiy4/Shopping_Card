6c324ce1c1f691d875e41237b2ae9ad2fa0f1341
Sergiy <sergiy0378@gmail.com>
2023-05-25 16:32:20 +0300

    make header animations

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,34 +1,85 @@
 import React from "react";
 import '../styles/Header.css';
-import { animated,useSpring } from "@react-spring/web";
+import { animated,useSpring, useSprings } from "@react-spring/web";
+import { useState } from "react";
+
 
 function Header(){
 
-    const [{textShadow}, set] = useSpring(() => ({translateZ:'0px',textShadow:'0px 0px 0px  rgb(0, 0, 0)'}));
+    const [styleName, set] = useSpring(() => ({textShadow:'0px 0px 0px  rgb(0, 0, 0)',top:'0%',left:'0%'}));
 
+    const data = [
+        {
+            name:'Shop'
+        },
+        {
+            name:'Contact'
+        },
+        {
+            name:'Shopping Card'
+        }
+    ]
+
+    const [index, setIndex] = useState(null);
+
+    const springs = useSprings(
+        data.length,
+        data.map((item,i)=>({
+            textShadow:i===index?'3px 3px 0px rgb(0, 0, 0)':'0px 0px 0px rgb(0, 0, 0)',
+            top:i===index? '-2.7%':'0%',
+            left:i === index?'-2.7%':'0%',
+            from:{
+                top:'0%',
+                left:'0%',
+                textShadow:'0px 0px 0px rgb(0, 0, 0)'
+            }
+        }))
+    );
+
+   
 
     return(
+
+
         <header className="header_stick">
         
             <animated.h1 
-            className="name_shop" 
-            style={{textShadow}}
+                className="name_shop" 
+                style={styleName}
             
-            onMouseEnter={()=>set({textShadow:'3px 3px 0px rgb(0, 0, 0)'})}
-            onMouseLeave={()=>set({textShadow:'0px 0px 0px rgb(0, 0, 0)'})}
-            >FakePlants</animated.h1>
+                onMouseEnter={()=>set({textShadow:'3px 3px 0px rgb(0, 0, 0)',top:'-2.7%',left:'-1%'})}
+                onMouseLeave={()=>set({textShadow:'0px 0px 0px rgb(0, 0, 0)',top:'0%',left:'0%'})}
+            >
+                FakePlants
+            </animated.h1>
 
-
-            
             <ul className="list">
-                <animated.li
-                >Shop</animated.li>
-                <li>Contact</li>
-                <li>Shopping cart</li>
+
+                {springs.map(
+                    (
+                        styleUl,
+                        i
+                    )=>(
+                        <animated.li
+                            onMouseEnter={()=>{
+                               setIndex(i);
+                            }}
+                            onMouseLeave={()=>{
+                                setIndex(null)
+                            }}
+                            key={i}
+                            style={styleUl}
+                        >
+                            {data[i].name}
+                        </animated.li>
+                    )
+
+                )}
                
             </ul>
         </header>
     )
 }
 
+// 
 export default Header
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -11,6 +11,8 @@ function Home(){
           rotateY:'0deg', 
           rotateX:'0deg', 
           rotateZ:'0deg',
+      
+
         },
         to: {
           opacity: 1,
@@ -18,11 +20,13 @@ function Home(){
           rotateY:'-30deg', 
           rotateX:'50deg', 
           rotateZ:'20deg',
+     
+
         },
         config: {
             mass: 5,
-            tension: 170,
-            friction: 60,
+            tension:170,
+            friction: 100,
           
             clamp:true
         },
@@ -30,8 +34,8 @@ function Home(){
     })
 // 
 
-    const [{translateZ,textShadow}, set] = useSpring(() => ({translateZ:'0px',textShadow:'0px 0px 0px  rgb(0, 0, 0)'}));
-    const [{boxShadow}, setShadow] = useSpring(() => ({boxShadow:'0px 0px 0px 0px rgb(0, 0, 0)'}));
+    const [textStyle, set] = useSpring(() => ({translateZ:'0px',textShadow:'0px 0px 0px  rgb(0, 0, 0)',top:'0%',left:'0%'}));
+    const [btnStyle, setShadow] = useSpring(() => ({boxShadow:'0px 0px 0px 0px rgb(0, 0, 0)', top:'0%',left:'0%'}));
     // const [{textShadow}, setTextShadow] = useSpring(() => ({textShadow:'0px 0px 0px 0px rgb(0, 0, 0)'}));
     return(
         <main className="Start_screen">
@@ -39,21 +43,22 @@ function Home(){
 
            
                 <div className="block"  >
-                    <animated.div  >
-                        <animated.h1  style={{translateZ,textShadow}}  className="Main_text"><animated.span  className="Main_span first_spain">Good</animated.span> <br/> Plant - good <br/><span className="Main_span">Mood</span></animated.h1>
+                    <animated.div >{/**/}
+                        <animated.h1  style={textStyle}  className="Main_text"><animated.span  className="Main_span first_spain">Good</animated.span> <br/> Plant - good <br/><span className="Main_span">Mood</span></animated.h1>
                         <animated.button 
                             className="Shop_Btn" 
-                            style={{boxShadow}} 
+                            style={btnStyle} 
                             onMouseEnter={()=>{
-                                setShadow({boxShadow:'15px 15px 0px 0px rgb(0, 0, 0)'})
-                                set({translateZ:'1000px',textShadow:'9px 5px 0px  rgb(0, 0, 0)'})
+                                setShadow({boxShadow:'15px 15px 0px 0px rgb(0, 0, 0)',top:'-2.7%',left:'-2.7%'})
+                                // 
+                                set({translateZ:'1000px',textShadow:'9px 5px 0px  rgb(0, 0, 0)',top:'-2.7%',left:'-2.7%'})
                                 // setTextShadow({textShadow:'6px 3px 1px rgb(0, 0, 0)'})
                             }
                             }
                             
                             onMouseLeave={()=>{
-                                setShadow({boxShadow:'0px 0px 0px 0px rgb(0, 0, 0)'})
-                                set({translateZ:'0px',textShadow:'0px 0px 0px rgb(0, 0, 0)'})
+                                setShadow({boxShadow:'0px 0px 0px 0px rgb(0, 0, 0)',top:'0%',left:'0%'})
+                                set({translateZ:'0px',textShadow:'0px 0px 0px rgb(0, 0, 0)',top:'0%',left:'0%'})
                             }
                             }
                         >Shop</animated.button>
